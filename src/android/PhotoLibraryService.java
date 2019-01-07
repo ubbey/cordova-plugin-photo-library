9c6c900970379845a40f1d4f23c018961476ed09
vence <vence@VenceZhous-macPro.local>
2019-01-07 15:08:50 +0800

    Add hashset

--- src/android/PhotoLibraryService.java
+++ src/android/PhotoLibraryService.java
@@ -39,6 +39,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.HashSet;
 
 public class PhotoLibraryService {
 
